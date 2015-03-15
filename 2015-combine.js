2b951b79f35daca568ba9affd18b3e6dbfb22510
Dominykas Blyžė <hello@dominykas.com>
2015-03-16 00:19:38 +0200

    final results; results in Kėdainiai

--- 2015-combine.js
+++ 2015-combine.js
@@ -1,33 +1,40 @@
 var fs = require("fs"),
 	_ = require("lodash");
 
-var t1 = _.indexBy(JSON.parse(fs.readFileSync("res-2015/t1.json")), "munip"),
-	t2 = JSON.parse(fs.readFileSync("res-2015/t2.json"));
+function combine(t1, t2) {
+	return _(t2).map(function (v) {
 
-var res = _(t2).map(function (v) {
+		var r1 = _.indexBy(t1[v.munip].res, "cand");
 
-	var r1 = _.indexBy(t1[v.munip].res, "cand");
+		return [
+			v.munip,
 
-	return [
-		v.munip,
-		+(t1[v.munip].res[0].cand != v.res[0].cand),
+			+(t1[v.munip].res[0].cand != v.res[0].cand),
 
-		"",
+			v.res[0].cand,
+			v.res[0].party,
+			+r1[v.res[0].cand].votes,
+			+v.res[0].votes,
+			((+v.res[0].votes) - (+r1[v.res[0].cand].votes)),
+			((+v.res[0].votes) - (+r1[v.res[0].cand].votes)) / (+r1[v.res[0].cand].votes),
 
-		v.res[0].cand,
-		v.res[0].party,
-		+r1[v.res[0].cand].votes,
-		+v.res[0].votes,
-		((+v.res[0].votes) - (+r1[v.res[0].cand].votes)),
+			"",
 
-		"",
+			v.res[1].cand,
+			v.res[1].party,
+			+r1[v.res[1].cand].votes,
+			+v.res[1].votes,
+			((+v.res[1].votes) - (+r1[v.res[1].cand].votes)),
+			((+v.res[1].votes) - (+r1[v.res[1].cand].votes)) / (+r1[v.res[0].cand].votes)
 
-		v.res[1].cand,
-		v.res[1].party,
-		+r1[v.res[1].cand].votes,
-		+v.res[1].votes,
-		((+v.res[1].votes) - (+r1[v.res[1].cand].votes)),
-	].join("\t");
-}).value().join("\n");
+		].join("\t");
+	}).value().join("\n");
+}
 
-console.log(res);
+fs.writeFileSync("res-2015/combined.tsv", combine(
+	_.indexBy(JSON.parse(fs.readFileSync("res-2015/t1.json")), "munip"),
+	JSON.parse(fs.readFileSync("res-2015/t2.json"))));
+
+fs.writeFileSync("res-2015/k-combined.tsv", combine(
+	_.indexBy(JSON.parse(fs.readFileSync("res-2015/k-t1.json")), "munip"),
+	JSON.parse(fs.readFileSync("res-2015/k-t2.json"))));
--- 2015-dl.js
+++ 2015-dl.js
@@ -6,28 +6,29 @@ function fetch(url) {
 	return Q.ninvoke(jsdom, "env", url, ["http://code.jquery.com/jquery.js"]);
 }
 
-function munip(url) {
+function munip(url, withParty) {
 	return fetch(url).then(function (w) {
 		var $ = w.$;
 
 		return $(".partydata3 td:first-child a").toArray().slice(0,2).map(function (lnk) {
+			var votesIdx = withParty ? 4 : 3;
 			return {
 				cand: $(lnk).text(),
-				party: $($(lnk).closest("tr").find("td")[1]).text(),
-				votes: $($(lnk).closest("tr").find("td")[4]).text()
+				party: withParty ? $($(lnk).closest("tr").find("td")[1]).text() : "-",
+				votes: $($(lnk).closest("tr").find("td")[votesIdx]).text()
 			}
 		})
 	});
 }
 
-function round(url) {
+function round(url, withParth) {
 	return fetch(url).then(function (w) {
 
 		var $ = w.$;
 
 		var promises = [];
 		$(".partydata3 td:first-child a").each(function (i, a) {
-			promises.push(munip(a.href).then(function (res) {
+			promises.push(munip(a.href, withParth).then(function (res) {
 				return {
 					munip: $(a).text(),
 					res: res
@@ -41,6 +42,7 @@ function round(url) {
 
 var done = [];
 
+// Lietuva
 done.push(round('http://www.2013.vrk.lt/2015_savivaldybiu_tarybu_rinkimai/output_lt/rezultatai_daugiamand_apygardose/rezultatai_daugiamand_apygardose1turas.html')
 	.then(function (t) {
 		fs.writeFileSync("res-2015/t1.json", JSON.stringify(t));
@@ -49,6 +51,17 @@ done.push(round('http://www.2013.vrk.lt/2015_savivaldybiu_tarybu_rinkimai/output
 done.push(round('http://www.2013.vrk.lt/2015_savivaldybiu_tarybu_rinkimai/output_lt/rezultatai_vienmand_apygardose2/rezultatai_vienmand_apygardose2turas.html')
 	.then(function (t) {
 		fs.writeFileSync("res-2015/t2.json", JSON.stringify(t));
+	}));
+
+// Kėdainiai
+done.push(round('http://www.2013.vrk.lt/2015_savivaldybiu_tarybu_rinkimai/output_lt/rezultatai_vienmand_apygardose/apygardos_rezultatai_apylinkese7779.html', false)
+	.then(function (t) {
+		fs.writeFileSync("res-2015/k-t1.json", JSON.stringify(t));
+	}));
+
+done.push(round('http://www.2013.vrk.lt/2015_savivaldybiu_tarybu_rinkimai/output_lt/rezultatai_vienmand_apygardose2/apygardos_rezultatai_apylinkese7843.html', false)
+	.then(function (t) {
+		fs.writeFileSync("res-2015/k-t2.json", JSON.stringify(t));
 	}));
 
 Q.all(done).done();

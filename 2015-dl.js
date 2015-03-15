eec364172a3e70b0102f3935c87f280f3440454b
Dominykas Blyžė <hello@dominykas.com>
2015-03-15 22:35:32 +0200

    2015 - raw data (~22:35); fix double promise push

--- 2015-dl.js
+++ 2015-dl.js
@@ -27,24 +27,28 @@ function round(url) {
 
 		var promises = [];
 		$(".partydata3 td:first-child a").each(function (i, a) {
-			promises.push(promises.push(munip(a.href).then(function (res) {
+			promises.push(munip(a.href).then(function (res) {
 				return {
 					munip: $(a).text(),
 					res: res
 				}
-			})));
+			}));
 		});
 
 		return Q.all(promises);
 	});
 }
 
-round('http://www.2013.vrk.lt/2015_savivaldybiu_tarybu_rinkimai/output_lt/rezultatai_daugiamand_apygardose/rezultatai_daugiamand_apygardose1turas.html')
+var done = [];
+
+done.push(round('http://www.2013.vrk.lt/2015_savivaldybiu_tarybu_rinkimai/output_lt/rezultatai_daugiamand_apygardose/rezultatai_daugiamand_apygardose1turas.html')
 	.then(function (t) {
 		fs.writeFileSync("res-2015/t1.json", JSON.stringify(t));
-	});
+	}));
 
-round('http://www.2013.vrk.lt/2015_savivaldybiu_tarybu_rinkimai/output_lt/rezultatai_vienmand_apygardose2/rezultatai_vienmand_apygardose2turas.html')
+done.push(round('http://www.2013.vrk.lt/2015_savivaldybiu_tarybu_rinkimai/output_lt/rezultatai_vienmand_apygardose2/rezultatai_vienmand_apygardose2turas.html')
 	.then(function (t) {
-		fs.writeFileSync("res-2015/t2.json", JSON.stringify(t2));
-	});
+		fs.writeFileSync("res-2015/t2.json", JSON.stringify(t));
+	}));
+
+Q.all(done).done();

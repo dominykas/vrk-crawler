68fa6a812b9953cbda9c8104c712576af0d6c2b1
Dominykas Blyžė <hello@dominykas.com>
2015-09-23 22:18:26 +0300

    use a different data sheet for VRK changes

--- 2016-gerrymandering-vien/2016-convert-vrk-changes.js
+++ 2016-gerrymandering-vien/2016-convert-vrk-changes.js
@@ -5,10 +5,12 @@ var normalize = require("./lib/normalize");
 csvParse(fs.readFileSync("./data/2015_09_15_perkelta.csv"), function (err, csv) {
 	fs.writeFileSync("./data/2016-vrk-changes.json", JSON.stringify(csv.map(function (row) {
 		return {
-			apygNum: row[1],
-			apygNew: normalize(row[2]),
-			apygOld: normalize(row[5]),
-			apyl: normalize(row[4])
+			apygNew: normalize(row[11]),
+			apygNewNum: parseInt(row[10], 10),
+			apygOld: normalize(row[8]),
+			apygOldNum: parseInt(row[7], 10),
+			apyl: normalize(row[2]),
+			apylNum: parseInt(row[1], 10)
 		}
 	}), null, 4));
 });

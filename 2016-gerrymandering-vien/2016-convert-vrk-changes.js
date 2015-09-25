40edd2e347c5dc24f23e6a713f898276e556dcd9
Dominykas Blyžė <hello@dominykas.com>
2015-09-25 12:37:26 +0200

    the items are starting to take a canonical shape a little bit

--- 2016-gerrymandering-vien/2016-convert-vrk-changes.js
+++ 2016-gerrymandering-vien/2016-convert-vrk-changes.js
@@ -5,12 +5,14 @@ var normalize = require("./lib/normalize");
 csvParse(fs.readFileSync("./data/2015_09_15_perkelta.csv"), function (err, csv) {
 	fs.writeFileSync("./data/2016-vrk-changes.json", JSON.stringify(csv.map(function (row) {
 		return {
-			apygNew: normalize(row[11]),
-			apygNewNum: parseInt(row[10], 10),
 			apygOld: normalize(row[8]),
 			apygOldNum: parseInt(row[7], 10),
-			apyl: normalize(row[2]),
-			apylNum: parseInt(row[1], 10)
+			apylOld: normalize(row[2]),
+			apylOldNum: parseInt(row[1], 10),
+			apygNew: normalize(row[11]),
+			apygNewNum: parseInt(row[10], 10),
+			apylNew: normalize(row[2]),
+			apylNewNum: parseInt(row[1], 10)
 		}
 	}), null, 4));
 });
